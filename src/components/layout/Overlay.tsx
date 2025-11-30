a101eb3bac8e803eabd8d9739b9c2290d6f61cc6
akashdigivizual-dev <akashdigivizual@gmail.com>
2025-11-30 20:58:51 +0530

    fix major issue

--- src/components/layout/Overlay.tsx
+++ src/components/layout/Overlay.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { heroContent } from '@/utils/constants';
+import { heroContent } from '../../utils/constants';
 
 const Overlay = () => {
   return (
